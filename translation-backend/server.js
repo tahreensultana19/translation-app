cfb00ee03f83c8b1e045ea37d87dda8b88c2e929
tahreen <tahreensultana19@gmail.com>
2024-08-08 16:58:26 -0400

    Second Commit

--- translation-backend/server.js
+++ translation-backend/server.js
@@ -64,6 +64,6 @@ app.post('/api/translations', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port,'0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
